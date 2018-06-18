5a7df138958a8b219fb0eb135cd5aaba56c42006
Yoni Tsafir <yoni@joytunes.com>
2018-06-18 21:49:52 +0300

    Made git marks in prompt more intuitive to me

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -22,10 +22,10 @@ set __fish_git_prompt_color_upstream_ahead green
 set __fish_git_prompt_color_upstream_behind red
 
 # Status Chars
-set __fish_git_prompt_char_dirtystate '⚡'
-set __fish_git_prompt_char_stagedstate '→'
-set __fish_git_prompt_char_untrackedfiles '☡'
-set __fish_git_prompt_char_stashstate '↩'
+set __fish_git_prompt_char_dirtystate 'D'
+set __fish_git_prompt_char_stagedstate 'A'
+set __fish_git_prompt_char_untrackedfiles 'U'
+set __fish_git_prompt_char_stashstate 'S'
 set __fish_git_prompt_char_upstream_ahead '+'
 set __fish_git_prompt_char_upstream_behind '-'
 
